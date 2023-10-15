3f91d0aa07e7152f45a0e26ed410c6b780508481
Huy (Been) <117251272+NguyenQuangHuy08@users.noreply.github.com>
2023-10-16 06:36:29 +0700

    Uyên cute

---
+++ navbar.js
@@ -0,0 +1,13 @@
+//Trả về phần tử đầu tiên có khớp hay khônbg
+const nav = document.querySelector(".nav"),
+  // searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+ 
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
